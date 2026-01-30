c7c45bcd309f1faf00e6fc1c3ec8b862f3d96c21
Lailaanouar <Lailaanouar1808@gmail.com>
2026-01-30 19:30:10 +0300

    Add app_constants.dart with secret REDACTED

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -12,7 +12,7 @@ class AppConstants {
   ];
 
   static const String paymentIntentPath = 'https://api.stripe.com/v1/payment_intents';
-  static const String secretKey = 'REDACTED';
+  static const String secretKey = 'sk_test_51SNEXcED9VjxD42VdJrT9uuySP2EkXltQA9bfv14G1tl73Cd2JN18RMciu5RpOqPU3AKVIAeMPR4KWvjTJdQElTV00RdQQRLmN';
   static const String publishableKey = 'pk_test_51SNEXcED9VjxD42VU83XQVzIiU8L2cSWbC5M0LVNsad4mmjAsDf24YyzEAcmh9eV4hrGVNzA8lpHwC6TlXGGJkLr00dJCVq9ZP';
 
 }
